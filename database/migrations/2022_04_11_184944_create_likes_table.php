7ae5badc80320de314694fcfbccda3bc167eea9c
Sergio <sergio.seblac@gmail.com>
2022-05-23 02:14:09 +0200

    fallo en las migraciones (solucionado)

--- database/migrations/2022_04_11_184944_create_likes_table.php
+++ database/migrations/2022_04_11_184944_create_likes_table.php
@@ -18,7 +18,7 @@ class CreateLikesTable extends Migration
             $table->foreignId('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreignId('table_id');
-            $table->foreign('table_id')->references('id')->on('table');
+            $table->foreign('table_id')->references('id')->on('table_exercises');
             $table->timestamps();
         });
     }
